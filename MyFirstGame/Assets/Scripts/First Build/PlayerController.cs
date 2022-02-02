9686aaaf9d5baabc3f26e23015d722795bbf027f
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2022-02-01 19:33:15 -0700

    Added Player Controller with Basic Movement.

--- MyFirstGame/Assets/Scripts/First Build/PlayerController.cs
+++ MyFirstGame/Assets/Scripts/First Build/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public float turnspeed = 50.0f;
+    public float hInput;
+    public float fInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+
+       // Makes the player move foward
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+       transform.Translate(Vector3.right * Time.deltaTime * turnspeed * hInput);
     }
 }
